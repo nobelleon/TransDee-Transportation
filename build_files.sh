69145ad0dce5266d4286e53e1ee5062be21c9b1d
unknown <nobelleon.86@gmail.com>
2026-02-06 23:26:38 +0700

    adding vercel

--- build_files.sh
+++ build_files.sh
@@ -4,15 +4,6 @@
 echo "Building project packages..."
 echo "Installing project requirements..."
 python3 -m pip install -r requirements.txt
-echo "Installing tailwindcss and dependencies..."
-python3 manage.py tailwind install
-
-echo "Building tailwindcss..."
-python3 manage.py tailwind build
-
-echo "Migrating Database..."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
