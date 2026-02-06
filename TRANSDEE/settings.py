e48dcdd3ac65757ed0063c62f850a03caff60579
unknown <nobelleon.86@gmail.com>
2026-02-06 22:10:25 +0700

    adding vercel

--- TRANSDEE/settings.py
+++ TRANSDEE/settings.py
@@ -118,7 +118,6 @@ USE_TZ = True
 STATIC_URL = 'transdeeapp/static/'
 
 MEDIA_URL = "images/"
-MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
