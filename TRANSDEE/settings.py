f1ff038dd6c76c03aeff34cd9b9abcf338b91601
unknown <nobelleon.86@gmail.com>
2026-02-06 21:49:41 +0700

    adding vercel

--- TRANSDEE/settings.py
+++ TRANSDEE/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7huof=4$=$3=r=hk0-rd+ja6l!mv#h6%c#!4k(0y80!i0qgfmg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -115,7 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'transdeeapp/static/'
+
+MEDIA_URL = "images/"
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
