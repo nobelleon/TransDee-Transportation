7785ae1c6d7f2681f6a194bd2b1a7c706707d623
unknown <nobelleon.86@gmail.com>
2026-02-06 21:59:47 +0700

    adding vercel

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,10 @@
 # build_files.sh
+#!/bin/bash
+echo "BUILD START"
+# Activate the virtual environment
+source venv/bin/activate
+# Install all dependencies
 pip install -r requirements.txt
+# Collect static files
 python3.9 manage.py collectstatic
+echo "BUILD END"
