33739272a188300a9a8210fd504b4c45fb0dc20b
unknown <nobelleon.86@gmail.com>
2026-02-06 22:17:53 +0700

    adding vercel

--- build_files.sh
+++ build_files.sh
@@ -5,6 +5,8 @@ echo "BUILD START"
 source venv/bin/activate
 # Install all dependencies
 pip install -r requirements.txt
-# Collect static files
-python3.9 manage.py collectstatic
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.9 -m venv venv
+# collect static files using the Python interpreter from venv
+python manage.py collectstatic --noinput
 echo "BUILD END"
